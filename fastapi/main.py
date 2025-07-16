f1c1814cdd55af2b6e4039cdfa6d2a8503c59b77
Jangyu Hong <jangyu.hong@namutech.co.kr>
2025-07-16 10:35:27 +0900

    fix: 서버 호출 주소를 localhost 에서 ip로 변경

--- fastapi/main.py
+++ fastapi/main.py
@@ -27,4 +27,4 @@ async def chat(req: QueryRequest):
         return {"error": str(e)}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8888)
